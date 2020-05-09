c87d21b8e927b6881e21775b73d09d1358f10432
Omar.elhani <omar.elhani1@gmail.com>
2020-05-10 00:22:36 +0200

    Casi terminado

--- app.py
+++ app.py
@@ -10,4 +10,42 @@ with open("MSX.json") as fichero:
 def inicio():
 	return render_template("inicio.html")
 
-app.run(debug=True)
+@app.route('/juegos',methods=["GET","POST"])
+def juegos():
+    if request.method == "GET":
+        lista_categoria=[]
+        for juegos in datos:
+            lista_categoria.append(juegos["categoria"])
+        return render_template('juegos.html', lista_categoria=set(lista_categoria))
+    else:
+        nombre_juego=request.form.get("nombre")
+        categoria=request.form.get("categorias")
+        lista_nombres=[]
+        lista_desarrolladores=[]
+        lista_ids=[]
+        lista_categoria=[]
+        for juegos in datos:
+            lista_categoria.append(juegos["categoria"])
+            if nombre_juego == "" and str(juegos["categoria"]) == categoria:
+                lista_desarrolladores.append(juegos["desarrollador"])
+                lista_nombres.append(juegos["nombre"])
+                lista_ids.append(juegos["id"])
+            elif str(juegos["nombre"]).startswith(nombre_juego) and str(juegos["categoria"]) == categoria:
+                lista_desarrolladores.append(juegos["desarrollador"])
+                lista_nombres.append(juegos["nombre"])
+                lista_ids.append(juegos["id"])
+        return render_template("juegos.html", nombre_juego=nombre_juego, lista_nombres=lista_nombres, lista_desarrolladores=lista_desarrolladores, lista_ids=lista_ids, lista_categoria=set(lista_categoria))
+
+  
+
+
+@app.route('/juego/<int:identificador>')
+def listajuegos(identificador):
+    for juegos in datos:
+        if juegos["id"] == identificador:
+            return render_template('listajuegos.html', juego=juegos)
+    return abort(404)
+
+
+port=os.environ["PORT"]
+app.run('0.0.0.0', int(port), debug=False)
